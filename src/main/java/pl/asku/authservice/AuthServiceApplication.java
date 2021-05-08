9cfc401e92318fedeabf2bed8addad7aa3e95b5e
sswrk <swierk.szymon99@gmail.com>
2021-05-08 17:05:08 +0200

    authorities table is now automatically populated

--- src/main/java/pl/asku/authservice/AuthServiceApplication.java
+++ src/main/java/pl/asku/authservice/AuthServiceApplication.java
@@ -1,13 +1,33 @@
 package pl.asku.authservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.asku.authservice.model.Authority;
+import pl.asku.authservice.repository.AuthorityRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadAuthorities(AuthorityRepository authorityRepository){
+        return args -> {
+            List<String> authorityNames = List.of("ROLE_USER", "ROLE_MODERATOR", "ROLE_ADMIN");
+            List<Authority> presentAuthorities = authorityRepository.findAll();
+            for(String authorityName : authorityNames){
+                Authority authority = new Authority(authorityName);
+                if(!presentAuthorities.contains(authority)){
+                    authorityRepository.save(authority);
+                }
+            }
+        };
     }
 
 }
--- src/main/java/pl/asku/authservice/model/Authority.java
+++ src/main/java/pl/asku/authservice/model/Authority.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Authority {
 
    @Id
