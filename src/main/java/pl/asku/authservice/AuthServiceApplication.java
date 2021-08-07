4449a61951b54877737581e48a1918789ddcda94
Szymon Świerk <swierk.szymon99@gmail.com>
2021-08-07 22:28:01 +0200

    added test users

--- src/main/java/pl/asku/authservice/AuthServiceApplication.java
+++ src/main/java/pl/asku/authservice/AuthServiceApplication.java
@@ -1,22 +1,42 @@
 package pl.asku.authservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.asku.authservice.model.Authority;
+import pl.asku.authservice.model.User;
 import pl.asku.authservice.repository.AuthorityRepository;
+import pl.asku.authservice.repository.UserRepository;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 @RefreshScope
 @EnableEurekaClient
 @EnableSwagger2
 public class AuthServiceApplication {
+
+    @Value("${test-data.user-username}")
+    private String testUserUsername;
+    @Value("${test-data.user-password}")
+    private String testUserPassword;
+
+    @Value("${test-data.moderator-username}")
+    private String testModeratorUsername;
+    @Value("${test-data.moderator-password}")
+    private String testModeratorPassword;
+
+    @Value("${test-data.admin-username}")
+    private String testAdminUsername;
+    @Value("${test-data.admin-password}")
+    private String testAdminPassword;
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
@@ -33,6 +53,40 @@ public class AuthServiceApplication {
                     authorityRepository.save(authority);
                 }
             }
+        };
+    }
+
+    @Bean CommandLineRunner loadTestUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
+
+        return args -> {
+            userRepository.save(
+                    new User(
+                            1L,
+                            testUserUsername,
+                            passwordEncoder.encode(testUserPassword),
+                            true,
+                            Set.of(new Authority("ROLE_USER"))
+                    )
+            );
+            userRepository.save(
+                    new User(
+                            2L,
+                            testModeratorUsername,
+                            passwordEncoder.encode(testModeratorPassword),
+                            true,
+                            Set.of(new Authority("ROLE_USER"), new Authority("ROLE_MODERATOR"))
+                    )
+            );
+            userRepository.save(
+                    new User(
+                            3L,
+                            testAdminUsername,
+                            passwordEncoder.encode(testAdminPassword),
+                            true,
+                            Set.of(new Authority("ROLE_USER"), new Authority("ROLE_MODERATOR"), new Authority("ROLE_ADMIN"))
+                    )
+            );
         };
     }
 
